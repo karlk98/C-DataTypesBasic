a3ed9c194f1dc6c17b54a438a103213d2fa27526
Karl Kinson <CMPKKINS@ljmu.ac.uk>
2019-01-31 12:46:41 +0000

    Simple program that uses two ints for an addition and saves the answer in a third int and has a string to store a message. The message and answer and message are then displayed to the console. (No user input, values are hard coded)

--- DataTypesBasic/Program.cs
+++ DataTypesBasic/Program.cs
@@ -10,6 +10,12 @@ namespace DataTypesBasic
     {
         static void Main(string[] args)
         {
+            int num1 = 5;
+            int num2 = 3;
+            int num3 = num1 + num2;
+            string message = "The answer is:";
+            Console.Write(message + num3);
+            Console.ReadKey();
         }
     }
 }
